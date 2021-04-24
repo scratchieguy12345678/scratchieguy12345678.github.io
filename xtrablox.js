a5558baa3f33cea09c3f8048a7dfda0f13623b48
scratchieguy12345678 <81265625+scratchieguy12345678@users.noreply.github.com>
2021-04-23 21:48:04 -0400

    Update xtrablox.js

--- xtrablox.js
+++ xtrablox.js
@@ -1,1 +1,29 @@
+class NitroBlock {
+    getInfo() {
+        return {
+            "id": "XtraBlocks",
+            "name": "XtraBlocks",
+            "blocks": [{
+                    "opcode": "xor",
+                    "blockType": "reporter",
+                    "text": "[bool1] xor [bool2]",
+                    "arguments": {
+                        "bool1": {
+                            "type": "Boolean",
+                            "defaultValue": ""
+                        },
+                        "bool2": {
+                            "type": "Boolean",
+                            "defaultValue": ""
+                        },
+                    }
+                },
+            }],
+        "menus": { //we will get back to this in a later tutorial
+        }
+    };
+    xor({bool1, bool2}) {
+        return string.substring(!bool1 != !bool2);
+    };
+}
 
